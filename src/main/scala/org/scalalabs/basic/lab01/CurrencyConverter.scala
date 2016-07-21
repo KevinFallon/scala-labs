5375d3396902e23e35d22efd96ceb0e58432d0b6
Kevin Fallon <kfallon2010@gmail.com>
2016-07-21 18:23:07 -0400

    Basic lab01 complete

---
+++ src/main/scala/org/scalalabs/basic/lab01/CurrencyConverter.scala
@@ -0,0 +1,17 @@
+package org.scalalabs.basic.lab01
+/**
+ * Classes used in [[org.scalalabs.basic.lab01.OOExerercise]]
+ */
+trait CurrencyConverter {
+  def toEuroCents(dollarCents: Int): Int
+}
+
+trait DefaultCurrencyConverter extends CurrencyConverter {
+  val conversionRate = 1.3598
+  def toEuroCents(dollarCents: Int): Int =
+    (dollarCents.toDouble * conversionRate).toInt
+  def toDollarCents(euroCents: Int): Int =
+    (euroCents.toDouble / conversionRate).toInt
+}
+
+object DefaultCurrencyConverter extends DefaultCurrencyConverter
---
+++ src/main/scala/org/scalalabs/basic/lab01/OOExercise.scala
@@ -0,0 +1,81 @@
+package org.scalalabs.basic.lab01
+import scala.language.implicitConversions
+/**
+ * The goal of this exercise is to get familiar basic OO constructs in scala
+ *
+ * Fix the code so that the unit test 'CurrencyExerciseTest' passes.
+ *
+ * In order for the tests to pass you need to do the following:
+ *
+ * Exercise 1:
+ * - Create a class Euro
+ * - Provide it with two constructor parameters: euro:Int, cents:Int
+ * - Provide the cents field with default value: 0
+ * - Provide an immutable field named: inCents that converts euro + cents into cents.
+ * - Create an object Euro with a factory method named: fromCents that creates an Euro based on cents.
+ * - Create a method named: + to the Euro class that adds another Euro
+ * - Create a method named: * to the Euro class that multiplies an Euro
+ *
+ * Exercise 2:
+ * - Create an abstract class Currency
+ * - Provide it with one constructor parameter: symbol:String
+ * - Extend the previously created Euro class from Currency
+ * - Override the toString method of Euro to represent the following String:
+ *   -> symbol + ': ' + euro + ',' + cents.  E.g: EUR 200,05
+ * - In case the cents are 0 use this representation:
+ *   -> symbol + ': ' + euro + ',--. E.g.: EUR 200.--
+ *
+ * Exercise 3:
+ * - Mix the Ordered trait in Euro
+ * - Implement the compare method
+ *
+ * Exercise 4:
+ * - Provide an implicit class that adds a *(euro:Euro) method to Int
+ * - Create a new currency Dollar
+ * - Provide a implicit conversion method that converts from Euro to Dollar using the
+ *   [[org.scalalabs.basic.lab01.DefaultCurrencyConverter]]
+ *
+ * Exercise 5:
+ * - Extend the conversion method from Euro to Dollar with an implicit parameter
+ *   of type [[org.scalalabs.basic.lab01.CurrencyConverter]]
+ * - Use the implicit CurrencyConverter to do the conversion.
+ */
+
+
+abstract class Currency(val symbol: String)
+
+
+// prefixing vars with val or var makes them visible outside of the class since they become fields
+class Euro (val euro: Int, val cents: Int = 0) extends Currency(symbol = "EUR") with Ordered[Euro] {
+  require(euro >= 0)
+  require(cents >= 0)
+  val inCents:Int = euro * 100 + cents
+
+  def +(other: Euro): Euro = Euro.fromCents(inCents + other.inCents)
+
+  def *(multiplier: Int): Euro = Euro.fromCents(inCents * multiplier)
+
+  def compare(that: Euro): Int = this.inCents - that.inCents
+
+  override def toString = {
+    val toStringCents:String =
+      if (cents == 0) "--"
+      else "%02d".format(cents)
+    symbol + ": " + euro + "," + toStringCents
+  }
+}
+
+object Euro {
+  def fromCents(cents: Int): Euro = {
+    new Euro( (cents - (cents % 100)) / 100, cents % 100)
+  }
+  implicit class EuroInt(val int: Int) extends AnyVal {
+    def *(euro: Euro) = euro * int
+  }
+
+  implicit def fromDollar(dollar: Dollar)(implicit converter: CurrencyConverter): Euro = Euro.fromCents(converter.toEuroCents(dollar.inCents))
+}
+
+class Dollar (dollars:Int, cents: Int) extends Currency(symbol = "USD") {
+  val inCents: Int = dollars * 100 + cents
+}
